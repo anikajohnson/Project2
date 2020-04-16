75010a17c94a9cec7c120bf851c89b1b0896342d
Anika Johnson <anikaleejohnson@gmail.com>
2020-04-16 15:04:50 -0500

    updated database and column capitalization issues, added sex ratio create table to pop_data

--- pop_data.sql
+++ pop_data.sql
@@ -1,10 +1,10 @@
 create table young_to_elder (
 	index INT PRIMARY KEY not null,
-	Country text,
-	Year text,
-	Young int,
-	Working_Age int,
-	Elder int
+	country text,
+	year text,
+	young int,
+	working_Age int,
+	elder int
 );
 
 CREATE TABLE un_sex_population (
@@ -31,5 +31,20 @@ index INT PRIMARY KEY not null,
     r_90_94 int,
     r_95_99 int,
     r_100_105 int,
-    Sex TEXT
+    sex TEXT
+);
+
+create table age_ratio_population (
+	index INT PRIMARY KEY not null,
+	year text,
+	r_15_year_olds int,
+	r_20_year_olds int,
+	r_30_year_olds int,
+	r_40_year_olds int,
+	r_50_year_olds int,
+	r_60_year_olds int,
+	r_70_year_olds int,
+	r_80_year_olds int,
+	r_90_year_olds int,
+	r_100_year_olds int
 );
